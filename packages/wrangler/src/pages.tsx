6e68a3baa8611fa9b8f570857c8eeb64960081fa
Greg Brimble <developer@gregbrimble.com>
2021-12-21 20:07:19 +0000

    Wait for initial build

--- packages/wrangler/src/pages.tsx
+++ packages/wrangler/src/pages.tsx
@@ -11,6 +11,7 @@ import { watch } from "chokidar";
 import { buildWorker } from "../lib/functions/buildWorker";
 import { Config, writeRoutesModule } from "../lib/functions/routes";
 import { generateConfigFromFileTree } from "../lib/functions/filepath-routing";
+import { BuildResult } from "esbuild";
 
 const RUNNING_PROCESSES: ChildProcess[] = [];
 const EXIT = (message?: string) => {
@@ -694,6 +695,8 @@ export const pages: BuilderCallback<unknown, unknown> = (yargs) => {
 
       let miniflareArgs: MiniflareOptions = {};
 
+      let functionsCompiler: Promise<BuildResult>;
+
       if (usingFunctions) {
         const scriptPath = join(tmpdir(), "./functionsWorker.js");
         const routesModule = join(tmpdir(), "./functionsRoutes.mjs");
@@ -714,7 +717,7 @@ export const pages: BuilderCallback<unknown, unknown> = (yargs) => {
           outfile: routesModule,
         });
 
-        const functionsCompiler = buildWorker({
+        functionsCompiler = buildWorker({
           routesModule,
           outfile: scriptPath,
           minify: false, // TODO: Expose option to enable
@@ -858,7 +861,11 @@ export const pages: BuilderCallback<unknown, unknown> = (yargs) => {
       console.log(`Serving at http://127.0.0.1:${port}/`);
 
       if (process.env.BROWSER !== "none") {
-        await open(`http://127.0.0.1:${port}/`);
+        if (functionsCompiler) {
+          await functionsCompiler.then(() => open(`http://127.0.0.1:${port}/`));
+        } else {
+          await open(`http://127.0.0.1:${port}/`);
+        }
       }
 
       process.on("SIGINT", () => {
